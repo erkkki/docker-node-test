0a416a093d1f2a81ade747d2f31915a61353919b
erkkki <erkheikki@hotmail.com>
2021-11-09 21:17:25 +0200

    workflow test 26

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+  res.send('Hello World, and 💓 for all.')
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
